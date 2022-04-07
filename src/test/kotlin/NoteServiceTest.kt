c6ce12f072d4b5f4123c08a43273135162b38c77
Aleksey Anokhin <anoxin-aleksey@bk.ru>
2022-04-07 21:01:43 +0300

    tests TODO

---
+++ src/test/kotlin/NoteServiceTest.kt
@@ -0,0 +1,46 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class NoteServiceTest {
+
+    @Test
+    fun addNote() {
+    }
+
+    @Test
+    fun createComment() {
+    }
+
+    @Test
+    fun deleteNote() {
+    }
+
+    @Test
+    fun deleteComment() {
+    }
+
+    @Test
+    fun editNote() {
+    }
+
+    @Test
+    fun editComment() {
+    }
+
+    @Test
+    fun getNotes() {
+    }
+
+    @Test
+    fun getById() {
+    }
+
+    @Test
+    fun getComments() {
+    }
+
+    @Test
+    fun restoreComment() {
+    }
+}
